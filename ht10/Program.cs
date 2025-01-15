8d2398072141297f726d8ade95a75a18968fa0eb
huchupuri <squaer120@gmail.com>
2025-01-15 20:40:54 +0300

    Добавьте файлы проекта.

---
+++ ht10/Program.cs
@@ -0,0 +1,24 @@
+﻿using MyLibrary;
+using System;
+
+namespace ht10
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<uint> accounts = new List<uint>()
+            {
+                new BankAccountFactory().CreatBankAccount(),
+                new BankAccountFactory().CreatBankAccount(1000),
+                new BankAccountFactory().CreatBankAccount(1000, AccountType.debit),
+                new BankAccountFactory().CreatBankAccount(AccountType.debit)
+            };
+            foreach (uint account in accounts)
+            {
+                Console.WriteLine(BankAccountFactory.accounts[account]);
+            }
+
+        }
+    }
+}
