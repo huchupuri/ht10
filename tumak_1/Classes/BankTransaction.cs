d8d74993fc7e5c36d36a374f71d6e6b4ac104119
huchupuri <squaer120@gmail.com>
2025-01-16 23:15:14 +0300

    fix

--- tumak_1/Classes/BankTransaction.cs
+++ tumak_1/Classes/BankTransaction.cs
@@ -11,7 +11,6 @@ namespace hw10.Classes
         public readonly DateTime transactionDate;
         public readonly decimal Amount;
 
-        // Конструктор
         public BankTransaction(decimal Amount)
         {
             this.transactionDate = DateTime.Now;
